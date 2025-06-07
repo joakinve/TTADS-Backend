fa610473fc0990458e83c5f881a52d3a637b4156
Joaquin <jokinve1997@gmail.com>
2025-06-07 19:34:29 -0300

    Cambios varios en el backend

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ const { sequelize } = require('./database/models/index');
 
 const PORT = process.env.PORT || 3000;
 
+
+
 http.listen(PORT, () => {
     console.log(`Running on a port: ${PORT}`);
     sequelize.sync({ force: false })
-        .then(() => { console.log('Conexion a DB exitosa'); })
+        .then(() => { 
+            console.log('Conexion a DB exitosa'); 
+            console.log(`Documentación de la API disponible en http://localhost:${PORT}/api-docs`);
+         })
         .catch(error => { console.log('Se ha producido un error', error) })
 });
